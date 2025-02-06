bf911c1104a932bfbb79a37ecb8f152e4c7869eb
MeetLunagariya <meetlunagariya47@gmail.com>
2025-02-06 15:32:02 +0530

    Updated Product_Image component

---
+++ src/product_page/productData.js
@@ -0,0 +1,37 @@
+import {
+  macbook_1,
+  macbook_2,
+  macbook_3,
+  macbook_4,
+  macbook_5,
+  macbook_6,
+} from "../assets/img";
+ 
+ const macbook_imgs = [
+    {
+      id: 1,
+      img: macbook_1,
+    },
+    {
+      id: 2,
+      img: macbook_2,
+    },
+    {
+      id: 3,
+      img: macbook_3,
+    },
+    {
+      id: 4,
+      img: macbook_4,
+    },
+    {
+      id: 5,
+      img: macbook_5,
+    },
+    {
+      id: 6,
+      img: macbook_6,
+    },
+  ];
+
+  export {macbook_imgs}
