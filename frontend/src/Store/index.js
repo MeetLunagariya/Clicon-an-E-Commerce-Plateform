1a23c91028b4211da6e87f1030170b33e1d02500
MeetLunagariya <meetlunagariya47@gmail.com>
2025-02-11 14:03:57 +0530

    Add Redux cart slice and configure store with cart reducer

--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -1,6 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cart/cartSlice";
 
 export default configureStore({
-  reducer: {},
-})
-
+  reducer: {
+    cart: cartReducer,
+  },
+});
